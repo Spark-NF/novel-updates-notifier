26b2bdbca5297c7da34145a01693391159c2a9fb
Spark-NF <spark_ps@hotmail.fr>
2018-06-07 01:57:42 +0200

    Switch from tabs to spaces

--- src/background/background.ts
+++ src/background/background.ts
@@ -3,159 +3,159 @@ import { sleep } from "../common/sleep";
 
 // tslint:disable-next-line
 interface ICustomWindow extends Window {
-	readingList: any;
+    readingList: any;
 }
 declare var window: ICustomWindow;
 
 // Storage methods
 /*var store = browser.storage.sync;
 try {
-	store.get(null)
+    store.get(null)
 } catch (e) {
-	store = browser.storage.local;
+    store = browser.storage.local;
 }*/
 const store = browser.storage.local;
 function setSettings(values: { [key: string]: any }) {
-	return store.set(values);
+    return store.set(values);
 }
 function getSettings() {
-	return store.get(null);
+    return store.get(null);
 }
 
 // Perform a series search
 async function search(query: string) {
-	const rq = await ajax("https://www.novelupdates.com/wp-admin/admin-ajax.php", "POST", {
-		action: "nd_ajaxsearchmain",
-		strType: "desktop",
-		strOne: query,
-	});
-	const parser = new DOMParser();
-	const xml = parser.parseFromString(rq.responseText, "text/html");
-	const links = xml.getElementsByClassName("a_search") as HTMLCollectionOf<HTMLLinkElement>;
+    const rq = await ajax("https://www.novelupdates.com/wp-admin/admin-ajax.php", "POST", {
+        action: "nd_ajaxsearchmain",
+        strType: "desktop",
+        strOne: query,
+    });
+    const parser = new DOMParser();
+    const xml = parser.parseFromString(rq.responseText, "text/html");
+    const links = xml.getElementsByClassName("a_search") as HTMLCollectionOf<HTMLLinkElement>;
 
-	const results = [];
-	for (const link of links) {
-		const img = link.getElementsByTagName("img")[0];
-		const name = link.getElementsByTagName("span")[0];
-		results.push({
-			name: name.innerHTML.trim(),
-			url: link.href,
-			img: img.src,
-		});
-	}
+    const results = [];
+    for (const link of links) {
+        const img = link.getElementsByTagName("img")[0];
+        const name = link.getElementsByTagName("span")[0];
+        results.push({
+            name: name.innerHTML.trim(),
+            url: link.href,
+            img: img.src,
+        });
+    }
 
-	return results;
+    return results;
 }
 
 // List management functions
 async function putInList(id: number) {
-	return ajax(`https://www.novelupdates.com/updatelist.php?sid=${id}&lid=0&act=move`);
+    return ajax(`https://www.novelupdates.com/updatelist.php?sid=${id}&lid=0&act=move`);
 }
 async function removeFromList(id: number) {
-	return ajax(`https://www.novelupdates.com/readinglist_update.php?rid=0&sid=${id}&checked=noo`);
+    return ajax(`https://www.novelupdates.com/readinglist_update.php?rid=0&sid=${id}&checked=noo`);
 }
 
 // Check if we are logged in
 async function checkLoginStatus() {
-	const cookies = await browser.cookies.getAll({ url: "https://www.novelupdates.com" });
-	for (const cookie of cookies) {
-		if (cookie.name.startsWith("wordpress_logged_in")) {
-			return true;
-		}
-	}
+    const cookies = await browser.cookies.getAll({ url: "https://www.novelupdates.com" });
+    for (const cookie of cookies) {
+        if (cookie.name.startsWith("wordpress_logged_in")) {
+            return true;
+        }
+    }
 
-	browser.browserAction.setBadgeText({ text: "OFF" });
-	browser.browserAction.setBadgeBackgroundColor({ color: "orange" });
-	return false;
+    browser.browserAction.setBadgeText({ text: "OFF" });
+    browser.browserAction.setBadgeBackgroundColor({ color: "orange" });
+    return false;
 }
 
 // Send login request
 function login(username: string, password: string) {
-	return ajax("https://www.novelupdates.com/login/", "POST", {
-		log: username,
-		pwd: password,
-	});
+    return ajax("https://www.novelupdates.com/login/", "POST", {
+        log: username,
+        pwd: password,
+    });
 }
 async function tryLogin() {
-	const settings = await getSettings();
-	if (!settings || !settings.username || !settings.password) {
-		return false;
-	}
-	login(settings.username, settings.password);
-	for (let i = 0; i < 30; ++i) {
-		if (await checkLoginStatus()) {
-			return true;
-		}
-		await sleep(100);
-	}
-	return false;
+    const settings = await getSettings();
+    if (!settings || !settings.username || !settings.password) {
+        return false;
+    }
+    login(settings.username, settings.password);
+    for (let i = 0; i < 30; ++i) {
+        if (await checkLoginStatus()) {
+            return true;
+        }
+        await sleep(100);
+    }
+    return false;
 }
 
 // Get the status of novels in the user's reading list
 async function loadReadingList() {
-	if (!await checkLoginStatus()) {
-		return;
-	}
+    if (!await checkLoginStatus()) {
+        return;
+    }
 
-	const rq = await ajax("https://www.novelupdates.com/reading-list/");
-	const parser = new DOMParser();
-	const xml = parser.parseFromString(rq.responseText, "text/html");
-	const rows = xml.getElementsByClassName("rl_links") as HTMLCollectionOf<HTMLTableRowElement>;
+    const rq = await ajax("https://www.novelupdates.com/reading-list/");
+    const parser = new DOMParser();
+    const xml = parser.parseFromString(rq.responseText, "text/html");
+    const rows = xml.getElementsByClassName("rl_links") as HTMLCollectionOf<HTMLTableRowElement>;
 
-	const novels = [];
-	let novelsWithChanges = 0;
+    const novels = [];
+    let novelsWithChanges = 0;
 
-	for (const row of rows) {
-		const cells = row.getElementsByTagName("td");
-		const checkboxInput = cells[0].getElementsByTagName("input")[0];
-		const novelLink = cells[1].getElementsByTagName("a")[0];
-		const statusLink = cells[2].getElementsByTagName("a")[0];
-		const latestIdInput = cells[2].getElementsByTagName("input")[0];
-		const latestLink = cells[3].getElementsByTagName("a")[0];
+    for (const row of rows) {
+        const cells = row.getElementsByTagName("td");
+        const checkboxInput = cells[0].getElementsByTagName("input")[0];
+        const novelLink = cells[1].getElementsByTagName("a")[0];
+        const statusLink = cells[2].getElementsByTagName("a")[0];
+        const latestIdInput = cells[2].getElementsByTagName("input")[0];
+        const latestLink = cells[3].getElementsByTagName("a")[0];
 
-		const novel = {
-			id: parseInt(row.dataset.sid, 10),
-			name: row.dataset.title,
-			url: novelLink.href,
-			status: {
-				id: parseInt(checkboxInput.value.substr(0, checkboxInput.value.indexOf(":")), 10),
-				name: statusLink.innerHTML,
-				url: statusLink.href,
-			},
-			latest: {
-				id: parseInt(latestIdInput.value, 10),
-				name: latestLink.innerHTML,
-				url: latestLink.href,
-			},
-		};
+        const novel = {
+            id: parseInt(row.dataset.sid, 10),
+            name: row.dataset.title,
+            url: novelLink.href,
+            status: {
+                id: parseInt(checkboxInput.value.substr(0, checkboxInput.value.indexOf(":")), 10),
+                name: statusLink.innerHTML,
+                url: statusLink.href,
+            },
+            latest: {
+                id: parseInt(latestIdInput.value, 10),
+                name: latestLink.innerHTML,
+                url: latestLink.href,
+            },
+        };
 
-		if (novel.status.id !== novel.latest.id) {
-			novelsWithChanges++;
-		}
-		novels.push(novel);
-	}
+        if (novel.status.id !== novel.latest.id) {
+            novelsWithChanges++;
+        }
+        novels.push(novel);
+    }
 
-	browser.browserAction.setBadgeText({ text: novelsWithChanges > 0 ? novelsWithChanges.toString() : "" });
-	browser.browserAction.setBadgeBackgroundColor({ color: "red" });
+    browser.browserAction.setBadgeText({ text: novelsWithChanges > 0 ? novelsWithChanges.toString() : "" });
+    browser.browserAction.setBadgeBackgroundColor({ color: "red" });
 
-	return novels;
+    return novels;
 }
 
 // Reading list accessor
 async function reloadReadingList() {
-	window.readingList = await loadReadingList();
+    window.readingList = await loadReadingList();
 }
 async function getReadingList() {
-	if (window.readingList === undefined) {
-		await reloadReadingList();
-	}
-	return window.readingList;
+    if (window.readingList === undefined) {
+        await reloadReadingList();
+    }
+    return window.readingList;
 }
 setInterval(reloadReadingList, 5 * 60 * 1000);
 
 // Initial load
 (async () => {
-	if (await checkLoginStatus() || await tryLogin()) {
-		reloadReadingList();
-	}
+    if (await checkLoginStatus() || await tryLogin()) {
+        reloadReadingList();
+    }
 })();
--- src/popup/main.ts
+++ src/popup/main.ts
@@ -14,109 +14,110 @@ const searchInput = document.getElementById("search").getElementsByTagName("inpu
 const searchResults = document.getElementById("search-results");
 
 async function removeNovel(id: number) {
-	await background.removeFromList(id);
-	const element = document.getElementById("novel-row-" + id);
-	element.parentElement.removeChild(element);
+    await background.removeFromList(id);
+    const element = document.getElementById("novel-row-" + id);
+    element.parentElement.removeChild(element);
 }
 
 async function displayNovels() {
-	const novels = await background.getReadingList();
+    const novels = await background.getReadingList();
 
-	// Empty the novels table first
-	let rowCount = novelsTable.rows.length;
-	while (--rowCount > 0) {
-		novelsTable.deleteRow(rowCount);
-	}
+    // Empty the novels table first
+    let rowCount = novelsTable.rows.length;
+    while (--rowCount > 0) {
+        novelsTable.deleteRow(rowCount);
+    }
 
-	// Populate the table
-	for (const novel of novels) {
-		const row = novelsTable.insertRow();
-		row.id = "novel-row-" + novel.id;
-		if (novel.status.id !== novel.latest.id) {
-			row.classList.add("table-warning");
-		}
-		const nameCell = row.insertCell();
-		nameCell.innerHTML = novel.name;
-		const readCell = row.insertCell();
-		readCell.innerHTML = novel.status.name;
-		const nextCell = row.insertCell();
-		const latestCell = row.insertCell();
-		latestCell.innerHTML = novel.latest.name;
-		const actionsCell = row.insertCell();
-		actionsCell.innerHTML = `<button class="btn btn-xs btn-danger" onclick="removeNovel("${novel.id}");">X</button>`;
-	}
+    // Populate the table
+    for (const novel of novels) {
+        const row = novelsTable.insertRow();
+        row.id = "novel-row-" + novel.id;
+        if (novel.status.id !== novel.latest.id) {
+            row.classList.add("table-warning");
+        }
+        const nameCell = row.insertCell();
+        nameCell.innerHTML = novel.name;
+        const readCell = row.insertCell();
+        readCell.innerHTML = novel.status.name;
+        const nextCell = row.insertCell();
+        const latestCell = row.insertCell();
+        latestCell.innerHTML = novel.latest.name;
+        const actionsCell = row.insertCell();
+        const btn = `<button class="btn btn-xs btn-danger" onclick="removeNovel("${novel.id}");">X</button>`;
+        actionsCell.innerHTML = btn;
+    }
 
-	loaderDiv.classList.add("hidden");
-	novelsDiv.classList.remove("hidden");
+    loaderDiv.classList.add("hidden");
+    novelsDiv.classList.remove("hidden");
 }
 
 // Button to refresh novel list
 novelsRefreshButton.onclick = async () => {
-	loaderText.innerHTML = "Refreshing novels...";
-	loaderDiv.classList.remove("hidden");
+    loaderText.innerHTML = "Refreshing novels...";
+    loaderDiv.classList.remove("hidden");
 
-	await background.reloadReadingList();
-	await displayNovels();
+    await background.reloadReadingList();
+    await displayNovels();
 };
 
 // Show search results input change
 let latestSearch = "";
 searchInput.oninput = async () => {
-	const val = searchInput.value.trim();
-	latestSearch = val;
-	if (val.length === 0) {
-		searchResults.innerHTML = "";
-	} else {
-		searchResults.innerHTML = "Loading results...";
-		const results = await background.search(val);
-		if (val !== latestSearch) {
-			return;
-		}
-		searchResults.innerHTML = "";
-		for (let i = 0; i < 5 && i < results.length; ++i) {
-			const result = results[i];
-			const li = document.createElement("li");
-			li.innerHTML = result.name;
-			searchResults.appendChild(li);
-		}
-	}
+    const val = searchInput.value.trim();
+    latestSearch = val;
+    if (val.length === 0) {
+        searchResults.innerHTML = "";
+    } else {
+        searchResults.innerHTML = "Loading results...";
+        const results = await background.search(val);
+        if (val !== latestSearch) {
+            return;
+        }
+        searchResults.innerHTML = "";
+        for (let i = 0; i < 5 && i < results.length; ++i) {
+            const result = results[i];
+            const li = document.createElement("li");
+            li.innerHTML = result.name;
+            searchResults.appendChild(li);
+        }
+    }
 };
 
 // Store credentials on login form submit
 loginForm.onsubmit = async (e) => {
-	e.preventDefault();
+    e.preventDefault();
 
-	loaderText.innerHTML = "Logging in...";
-	loaderDiv.classList.remove("hidden");
+    loaderText.innerHTML = "Logging in...";
+    loaderDiv.classList.remove("hidden");
 
-	await background.setSettings({
-		username: loginUsername.value,
-		password: loginPassword.value,
-	});
+    await background.setSettings({
+        username: loginUsername.value,
+        password: loginPassword.value,
+    });
 
-	if (await background.tryLogin()) {
-		loginFormError.classList.add("hidden");
-		loginDiv.classList.add("hidden");
+    if (await background.tryLogin()) {
+        loginFormError.classList.add("hidden");
+        loginDiv.classList.add("hidden");
 
-		await background.reloadReadingList();
-		await displayNovels();
-	} else {
-		loginFormError.innerHTML = "Login failure";
-		loginFormError.classList.remove("hidden");
+        await background.reloadReadingList();
+        await displayNovels();
+    } else {
+        loginFormError.innerHTML = "Login failure";
+        loginFormError.classList.remove("hidden");
 
-		loaderDiv.classList.add("hidden");
-	}
+        loaderDiv.classList.add("hidden");
+    }
 
-	return false;
+    return false;
 };
 
 // Show novels or login form on popup load
 (async () => {
-	if (await background.checkLoginStatus()) {
-		loaderText.innerHTML = "Loading reading list...";
-		await displayNovels();
-	} else {
-		loaderDiv.classList.add("hidden");
-		loginDiv.classList.remove("hidden");
-	}
+    if (await background.checkLoginStatus()) {
+        loaderText.innerHTML = "Loading reading list...";
+        await displayNovels();
+    } else {
+        loaderDiv.classList.add("hidden");
+        loginDiv.classList.remove("hidden");
+    }
 })();
