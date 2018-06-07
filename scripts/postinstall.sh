5cd330691094acd08588b2d48c0c15e7d72e63f3
Spark-NF <spark_ps@hotmail.fr>
2018-06-07 16:19:42 +0200

    Add map files to vendor files

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -7,6 +7,8 @@ mkdir -p "src/vendor/fonts"
 mkdir -p "src/vendor/js"
 
 cp -f "node_modules/bootstrap/dist/css/bootstrap.min.css" "src/vendor/css/"
+cp -f "node_modules/bootstrap/dist/css/bootstrap.min.css.map" "src/vendor/css/"
 cp -f "node_modules/font-awesome/css/font-awesome.min.css" "src/vendor/css/"
 cp -fr "node_modules/font-awesome/fonts/" "src/vendor/"
 cp -f "node_modules/webextension-polyfill/dist/browser-polyfill.min.js" "src/vendor/js/"
+cp -f "node_modules/webextension-polyfill/dist/browser-polyfill.min.js.map" "src/vendor/js/"
