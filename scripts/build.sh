4b353cffd539051ea19a90fd06ea28d0de4ebc3d
Spark-NF <spark_ps@hotmail.fr>
2018-06-08 11:13:57 +0200

    Add basic sidebar

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,4 +3,5 @@
 tsc -p "src/tsconfig.json"
 
 rollup "src/background/background.js" --o "src/background/bundle.js" --no-treeshake --f esm
-rollup "src/popup/main.js" --o "src/popup/bundle.js" --f esm
+rollup "src/popup/popup.js" --o "src/popup/bundle.js" --f esm
+rollup "src/sidebar/sidebar.js" --o "src/sidebar/bundle.js" --f esm
