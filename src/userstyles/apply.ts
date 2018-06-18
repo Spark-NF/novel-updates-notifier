16852d7cea7cb62069cbf51188ebfadd7d45dc63
Spark-NF <spark_ps@hotmail.fr>
2018-06-19 00:21:59 +0200

    Add basic wuxiaworld stylesheet

--- src/userstyles/apply.ts
+++ src/userstyles/apply.ts
@@ -1,12 +1,17 @@
+const domains = [
+    "webnovel.com",
+    "wuxiaworld.com",
+];
+
 document.addEventListener("DOMContentLoaded", () => {
-
-    if (location.hostname.includes("webnovel.com")) {
-        const head = document.getElementsByTagName("head")[0];
-        const link = document.createElement("link");
-        link.setAttribute("rel", "stylesheet");
-        link.setAttribute("type", "text/css");
-        link.setAttribute("href", browser.extension.getURL("src/userstyles/css/webnovel.com.css"));
-        head.appendChild(link);
+    for (const domain of domains) {
+        if (location.hostname.includes(domain)) {
+            const head = document.getElementsByTagName("head")[0];
+            const link = document.createElement("link");
+            link.setAttribute("rel", "stylesheet");
+            link.setAttribute("type", "text/css");
+            link.setAttribute("href", browser.extension.getURL("src/userstyles/css/" + domain + ".css"));
+            head.appendChild(link);
+        }
     }
-
 });
