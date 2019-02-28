a2e9c37bed072227cb21cd80cac7e3b63833eb85
Spark-NF <spark_ps@hotmail.fr>
2019-02-28 15:40:05 +0100

    Fix watch script to not re-build everything

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -2,7 +2,7 @@
 
 tsc -w -p "src/tsconfig.json" &
 
-rollup --w "src/background/background.js" --o "src/background/bundle.js" --no-treeshake --f esm &
-rollup --w "src/popup/popup.js" --o "src/popup/bundle.js" --no-treeshake --f esm &
-rollup --w "src/sidebar/sidebar.js" --o "src/sidebar/bundle.js" --no-treeshake --f esm &
-rollup --w "src/userstyles/apply.js" --o "src/userstyles/bundle.js" --no-treeshake --f esm
+rollup "dist/background/background.js" -o "src/background/bundle.js" --no-treeshake -f esm -w &
+rollup "dist/popup/popup.js" -o "src/popup/bundle.js" --no-treeshake -f esm -w &
+rollup "dist/sidebar/sidebar.js" -o "src/sidebar/bundle.js" --no-treeshake -f esm -w &
+rollup "dist/userstyles/apply.js" -o "src/userstyles/bundle.js" --no-treeshake -f esm -w
