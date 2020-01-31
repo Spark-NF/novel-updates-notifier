656a86f926e20ebcebc51637a08eb881fab6824d
Spark-NF <spark_ps@hotmail.fr>
2020-02-01 00:10:09 +0100

    Use Vue runtime to get rid of template compilation using eval (fix #21)

--- src/options/options.ts
+++ src/options/options.ts
@@ -1,9 +1,6 @@
-import Vue from "vue";
 import { Settings } from "../common/Settings";
 import { Storage } from "../common/Storage";
-import OptionsGeneral from "../options/components/OptionsGeneral.vue";
-
-Vue.component("options-general", OptionsGeneral);
+import Options from "../options/components/Options.vue";
 
 const storage = new Storage();
 const settings = new Settings(storage);
@@ -12,16 +9,8 @@ const settings = new Settings(storage);
     await storage.init();
     await settings.preload();
 
-    const app = new Vue({
+    const app = new Options({
         el: "#app",
-        data: {
-            hasSidebar: browser.sidebarAction !== undefined,
-            hasBadge: !!(browser.browserAction && browser.browserAction.setBadgeText),
-        },
-        computed: {
-            settings(): Settings {
-                return settings;
-            },
-        },
+        propsData: { settings },
     });
 })();
