ae09629f2bdc6ca29d311c2f88fd423049d072c3
Spark-NF <spark_ps@hotmail.fr>
2024-11-16 16:19:56 +0100

    fix: stop using deprecated extension.getURL method

--- src/common/notifications.ts
+++ src/common/notifications.ts
@@ -1,7 +1,7 @@
 export async function notify(title: string, message: string): Promise<string> {
     return browser.notifications.create({
         type: "basic" as any,
-        iconUrl: browser.extension.getURL("icons/icon-48.png"),
+        iconUrl: browser.runtime.getURL("icons/icon-48.png"),
         title,
         message,
     });
