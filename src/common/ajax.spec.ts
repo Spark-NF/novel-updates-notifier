46aa57bf77507fd2a3da2c164942313eeb4c0275
Spark-NF <spark_ps@hotmail.fr>
2024-11-16 22:22:58 +0100

    fix: automatically copy partitioned cookies (fix #32)

--- src/common/ajax.spec.ts
+++ src/common/ajax.spec.ts
@@ -1,4 +1,4 @@
-import { ajax, objectToParams } from "./ajax";
+import { ajax, objectToParams, syncPartitionedCookies } from "./ajax";
 
 describe("objectToParams", () => {
     it("Returns an empty string for non-objects", () => {
@@ -17,8 +17,46 @@ describe("objectToParams", () => {
     });
 });
 
+describe("syncPartitionedCookies", () => {
+    it("Does nothing if all the cookies are already available", async () => {
+        const nonPartitionedCookies = [{name: "test"}];
+        const allCookies = [{name: "test"}, {name: "test", partitionKey: "partition"}];
+        const mockSet = jest.fn()
+        window.browser = {
+            cookies: {
+                getAll: jest.fn().mockResolvedValueOnce(nonPartitionedCookies).mockResolvedValueOnce(allCookies),
+                set: mockSet,
+            },
+        } as any;
+
+        await syncPartitionedCookies("https://github.com");
+        expect(mockSet).not.toHaveBeenCalled();
+    });
+
+    it("Sets the value without a partition key if not found", async () => {
+        const allCookies = [{name: "test", partitionKey: "partition"}];
+        const mockSet = jest.fn()
+        window.browser = {
+            cookies: {
+                getAll: jest.fn().mockResolvedValueOnce([]).mockResolvedValueOnce(allCookies),
+                set: mockSet,
+            },
+        } as any;
+
+        await syncPartitionedCookies("https://github.com");
+        expect(mockSet).toHaveBeenCalledWith({ url: "https://github.com", name: "test" });
+    });
+});
+
 describe("ajax", () => {
     it("Returns the response of a GET request", async () => {
+        window.browser = {
+            cookies: {
+                getAll: jest.fn().mockResolvedValue([]),
+                set: jest.fn(),
+            },
+        } as any;
+
         const rq = await ajax("https://raw.githubusercontent.com/Spark-NF/novel-updates-notifier/master/README.md");
         expect(rq.responseText).toContain("Novel Updates Notifier");
     });
--- src/common/ajax.ts
+++ src/common/ajax.ts
@@ -1,3 +1,5 @@
+import { clone } from "./clone";
+
 export function objectToParams(obj: any): string {
     let str = "";
     if (typeof obj === "object") {
@@ -13,7 +15,28 @@ export function objectToParams(obj: any): string {
     return str;
 }
 
-export function ajax(url: string, method?: "GET" | "POST", data?: any, contentType?: string): Promise<XMLHttpRequest> {
+export async function syncPartitionedCookies(url: string): Promise<void> {
+    /**
+     * Clone partitioned cookies as non-partitioned to allow access in XHR / fetch calls.
+     * @see https://github.com/Spark-NF/novel-updates-notifier/issues/32
+     */
+    const nonPartitionedCookies = await browser.cookies.getAll({url});
+    const nonPartitionedCookieNames = nonPartitionedCookies.map(cookie => cookie.name)
+    const allCookies = await browser.cookies.getAll({url, partitionKey: {}});
+
+    for (const cookie of allCookies) {
+        if (!nonPartitionedCookieNames.includes(cookie.name)) {
+            const copy = clone(cookie) as any;
+            for (const key of ["partitionKey", "hostOnly", "session"]) {
+                delete copy[key];
+            }
+            await browser.cookies.set({ url, ...copy });
+            console.debug("Cloned partitioned cookie", cookie.name);
+        }
+    }
+}
+
+export function ajaxXHR(url: string, method?: "GET" | "POST", data?: any, contentType?: string): Promise<XMLHttpRequest> {
     return new Promise<XMLHttpRequest>((resolve, reject) => {
         const xhr = new XMLHttpRequest();
         xhr.onload = function() {
@@ -33,4 +56,9 @@ export function ajax(url: string, method?: "GET" | "POST", data?: any, contentTy
             xhr.send();
         }
     });
+}
+
+export async function ajax(url: string, method?: "GET" | "POST", data?: any, contentType?: string): Promise<XMLHttpRequest> {
+    await syncPartitionedCookies(url);
+    return ajaxXHR(url, method, data, contentType);
 }
